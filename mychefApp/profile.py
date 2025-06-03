eddabf8f9ed397deb6fc0997d454bbb073a871fc
GKL-Development <admin@gkldevelopment.com>
2025-06-03 21:36:24 +0200

    Implemented cookies integration for session validity

--- mychefApp/profile.py
+++ mychefApp/profile.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+ss_user = st.session_state.user_instance
+
 ####################################### Profile ####################################################
-st.title(f"{st.session_state.user_instance.firstName}'s profile 👤")
+st.title(f"{ss_user.firstName}'s profile 👤")
 st.divider()
+st.subheader("My Information:")
+firstname, lastname = st.columns(2, vertical_alignment="top", gap="large", border=False)
+firstname.text_input("Firstname:", value=ss_user.firstName)
+lastname.text_input("Lastname", value=ss_user.lastName)
