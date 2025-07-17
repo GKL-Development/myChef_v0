426379f81c33ed7839ce4ee179dec3bce8224902
GKL-Development <admin@gkldevelopment.com>
2025-07-17 21:17:23 +0200

    Trying to fix path error for functions.generateMealPlans

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="mychef_v0",
     version="0.1.0",
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
+    packages=find_packages(),
 )
